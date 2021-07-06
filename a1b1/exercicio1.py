c76e9ede03104fa6207b196d543cb8dd230420dc
henrique117 <86057591+henrique117@users.noreply.github.com>
2021-07-05 22:45:19 -0300

    Delete a1b1 directory

--- a1b1/exercicio1.py
+++
@@ -1,5 +0,0 @@
-print ('Informe, em metros, a largura e o comprimento de uma sala para que se calcule a sua área: ')
-l = int(input('Largura: '))
-c = int(input('Comprimento: '))
-a= l * c
-print ('Área da sala: ',a)
--- a1b1/exercicio3.py
+++
@@ -1,8 +0,0 @@
-print ("Informe três números diferentes para serem ordenados de forma crescente:")
-h = 0
-vetor = list(range(3))
-while h < 3:
-    vetor [h] = int(input('Informe um numero:'))
-    h = h+1
-vetor.sort()
-print('Os números ordenados de forma crescente correspondem a: ', vetor)
