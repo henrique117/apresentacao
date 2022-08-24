52c83753772a619a3cd5926c18e4a5d9e8a2fd06
henrique117 <86057591+henrique117@users.noreply.github.com>
2022-08-23 23:18:25 -0300

    Delete ProPlantae directory

--- ProPlantae/src/interfaces/Button.interface.ts
+++
@@ -1,5 +0,0 @@
-export interface ButtonProps {
-    onPress: () => void
-    title: string
-    type: 'google' | 'face' | 'email'
-}
--- ProPlantae/src/navigations/index.tsx
+++
@@ -1,3 +0,0 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import LoginNavigation from "./login.navigation";
