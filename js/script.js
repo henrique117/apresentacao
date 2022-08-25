7229d1b15b5d0822d247543f74d0716d307e4002
henrique <hassis.moreira177@hotmail.com>
2022-08-24 23:22:37 -0300

    Ajustes

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ var copyright = document.querySelector(".copy")
 var score = document.querySelector(".score")
 var counter = 0
 var gameOver = false
-var tempo = 4
+var tempo = 4000
 
 const jogar = () => {
 
@@ -17,95 +17,120 @@ const jogar = () => {
     start()
 
     setTimeout(() => {
-        const loop = setInterval(() => {
 
-            const canoPos = cano.offsetLeft
-            const squarePos = +window.getComputedStyle(mario).bottom.replace('px', '')
-        
-            if (canoPos <= 400 && canoPos > 330 && squarePos < 60) {
-        
-                cano.style.animation = 'none'
-                mario.style.animation = 'none'
-        
-                cano.style.left = `${canoPos}px`
-                mario.style.bottom = `${squarePos}px`
-        
-                mario.src = './images/game-over.png'
-                mario.style.width = '47px'
-                mario.style.height = '60px'
-                mario.style.marginLeft = '50px'
-        
-                gameOver = true
-        
-            }
-        
+        const loop2 = setInterval(() => {
+
             if (!gameOver) {
                 score.innerHTML = counter
                 counter++
-            } else {
-                over.innerHTML = 'GAME OVER'
-                copyright.innerHTML = '@henrique_moreira177'
             }
-    
-            if (counter > 1) {
-                if (counter > 200 && counter < 400 && canoPos < -30) {
-                    tempo = 3.5
+
+            const loop = setInterval(() => {
+
+                const canoPos = cano.offsetLeft
+                const squarePos = +window.getComputedStyle(mario).bottom.replace('px', '')
+            
+                if (canoPos <= 400 && canoPos > 330 && squarePos < 60) {
+            
                     cano.style.animation = 'none'
-                    cano.offsetHeight
-                    cano.style.animation = null
-                    cano.style.animationDuration = `${tempo}s`
-                }
+                    mario.style.animation = 'none'
+            
+                    cano.style.left = `${canoPos}px`
+                    mario.style.bottom = `${squarePos}px`
+            
+                    mario.src = './images/game-over.png'
+                    mario.style.width = '47px'
+                    mario.style.height = '60px'
+                    mario.style.marginLeft = '50px'
+            
+                    gameOver = true
     
-                if (counter > 400 && counter < 600 && canoPos < -30) {
-                    tempo = 3
-                    cano.style.animation = 'none'
-                    cano.offsetHeight
-                    cano.style.animation = null
-                    cano.style.animationDuration = `${tempo}s`
-                }
+                    over.innerHTML = 'GAME OVER'
+                    copyright.innerHTML = '@henrique_moreira177'
     
-                if (counter > 600 && counter < 800 && canoPos < -30) {
-                    tempo = 2.5
-                    cano.style.animation = 'none'
-                    cano.offsetHeight
-                    cano.style.animation = null
-                    cano.style.animationDuration = `${tempo}s`
+                    return(gameOver)
+            
                 }
-    
-                if (counter > 800 && counter < 1000 && canoPos < -30) {
-                    tempo = 2
-                    cano.style.animation = 'none'
-                    cano.offsetHeight
-                    cano.style.animation = null
-                    cano.style.animationDuration = `${tempo}s`
-                }
-    
-                if (counter > 1000 && counter < 1200 && canoPos < -30) {
-                    tempo = 1.5
-                    cano.style.animation = 'none'
-                    cano.offsetHeight
-                    cano.style.animation = null
-                    cano.style.animationDuration = `${tempo}s`
-                }
-    
-                if (counter > 1200 && counter < 1400 && canoPos < -30) {
-                    tempo = 1
-                    cano.style.animation = 'none'
-                    cano.offsetHeight
-                    cano.style.animation = null
-                    cano.style.animationDuration = `${tempo}s`
-                }
-    
-                if (counter > 1400 && counter < 1600 && canoPos < -30) {
-                    tempo = 0.5
-                    cano.style.animation = 'none'
-                    cano.offsetHeight
-                    cano.style.animation = null
-                    cano.style.animationDuration = `${tempo}s`
-                }
-            }
+                    
+                if (counter > 1) {
+                    if (counter > 200 && counter < 400 && canoPos < -30) {
+                        tempo = 3500
+                        cano.style.animation = 'none'
+                        cano.offsetHeight
+                        cano.style.animation = null
+                        cano.style.animationDuration = `${tempo}ms`
+                    }
         
-        }, 50)
+                    if (counter > 400 && counter < 600 && canoPos < -30) {
+                        tempo = 3000
+                        cano.style.animation = 'none'
+                        cano.offsetHeight
+                        cano.style.animation = null
+                        cano.style.animationDuration = `${tempo}ms`
+                    }
+        
+                    if (counter > 600 && counter < 800 && canoPos < -30) {
+                        tempo = 2500
+                        cano.style.animation = 'none'
+                        cano.offsetHeight
+                        cano.style.animation = null
+                        cano.style.animationDuration = `${tempo}ms`
+                    }
+        
+                    if (counter > 800 && counter < 1000 && canoPos < -30) {
+                        tempo = 2000
+                        cano.style.animation = 'none'
+                        cano.offsetHeight
+                        cano.style.animation = null
+                        cano.style.animationDuration = `${tempo}ms`
+                    }
+        
+                    if (counter > 1000 && counter < 1200 && canoPos < -30) {
+                        tempo = 1500
+                        cano.style.animation = 'none'
+                        cano.offsetHeight
+                        cano.style.animation = null
+                        cano.style.animationDuration = `${tempo}ms`
+                    }
+        
+                    if (counter > 1200 && counter < 1400 && canoPos < -30) {
+                        tempo = 1000
+                        cano.style.animation = 'none'
+                        cano.offsetHeight
+                        cano.style.animation = null
+                        cano.style.animationDuration = `${tempo}ms`
+                    }
+        
+                    if (counter > 1400 && counter < 1600 && canoPos < -30) {
+                        tempo = 500
+                        cano.style.animation = 'none'
+                        cano.offsetHeight
+                        cano.style.animation = null
+                        cano.style.animationDuration = `${tempo}ms`
+                    }
+    
+                    if (counter > 1600 && counter < 1800 && canoPos < -30) {
+                        tempo = 300
+                        cano.style.animation = 'none'
+                        cano.offsetHeight
+                        cano.style.animation = null
+                        cano.style.animationDuration = `${tempo}ms`
+                    }
+    
+                    if (counter > 1800 && counter < 2000 && canoPos < -30) {
+                        tempo = 200
+                        cano.style.animation = 'none'
+                        cano.offsetHeight
+                        cano.style.animation = null
+                        cano.style.animationDuration = `${tempo}ms`
+                    }
+    
+                }
+            
+            }, 5)
+
+        }, 100)
+
     }, 2000)
 
 }
@@ -119,6 +144,10 @@ const start = () => {
         mario.style.left = "300px"
         mario.style.removeProperty('animation')
     }, 1000)
+}
+
+const stop = () => {
+    gameOver = true
 }
 
 const jump = () => {
